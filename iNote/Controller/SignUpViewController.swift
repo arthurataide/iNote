e8a4ca42004932a736c3b455b41e03de542b6282
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2020-06-14 06:24:16 -0400

    Sign up validations

--- iNote/Controller/SignUpViewController.swift
+++ iNote/Controller/SignUpViewController.swift
@@ -42,14 +42,62 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func signUpTapped(_ sender: UIButton) {
-        signUp(username: usernameTextField.text!,
-               password: passwordTextField.text!,
-               email: emailTextField.text!,
-               givenName: firstNameTextField.text!,
-               familyName: lastNameTextField.text!)
+        if (vaidateFields()){
+            signUp(username: usernameTextField.text!,
+                   password: passwordTextField.text!,
+                   email: emailTextField.text!,
+                   givenName: firstNameTextField.text!,
+                   familyName: lastNameTextField.text!)
+            
+        }
     }
     @IBAction func signUpGoogleTapped(_ sender: UIButton) {
         
+    }
+    
+    func vaidateFields() -> Bool {
+        if (usernameTextField.text!.isEmpty){
+            showMessage("Sign Up", "Username is required.", "OK")
+            return false
+        }
+        
+        if (firstNameTextField.text!.isEmpty){
+            showMessage("Sign Up", "First Name is required.", "OK")
+            return false
+        }
+        
+        if (lastNameTextField.text!.isEmpty){
+            showMessage("Sign Up", "Last Name is required.", "OK")
+            return false
+        }
+        
+        if (emailTextField.text!.isEmpty){
+            showMessage("Sign Up", "Email is required.", "OK")
+            return false
+        }
+        
+        if (passwordTextField.text!.isEmpty){
+            showMessage("Sign Up", "Password is required.", "OK")
+            return false
+        }
+        
+        if (passwordTextField.text! != confirmPasswordTextField.text!){
+            showMessage("Sign Up", "Please make sure passwords match.", "OK")
+            return false
+        }
+        
+        if (!emailTextField.text!.contains("@") || !emailTextField.text!.contains(".")){
+            showMessage("Sign Up", "Please enter a valid Email address. For example jhon@email.com.", "OK")
+            return false
+        }
+        
+        return true
+    }
+    
+    func showMessage(_ title:String, _ message:String, _ actionMessage:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString(actionMessage, comment: actionMessage), style: .default))
+        self.present(alert, animated: true, completion: nil)
     }
     
     
@@ -62,15 +110,32 @@ class SignUpViewController: UIViewController {
         _ = Amplify.Auth.signUp(username: username, password: password, options: options) { result in
             switch result {
             case .success(let signUpResult):
+                var message = ""
                 if case let .confirmUser(deliveryDetails, _) = signUpResult.nextStep {
+                    message = "SignUp Complete. Must confirm account."
                     print("Delivery details \(String(describing: deliveryDetails))")
                 } else {
-                    print("SignUp Complete")
+                    message = "SignUp Complete"
                 }
+                DispatchQueue.main.async {
+                    self.successSignUp(message)
+                }
+                print(message)
             case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showMessage("Sign Up", "An error occured while registering a user \(error)", "OK")
+                }
                 print("An error occured while registering a user \(error)")
             }
         }
+    }
+    
+    func successSignUp(_ message:String) {
+        let alert = UIAlertController(title: "Sign Up", message:message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default,handler: { alert -> Void in
+            self.navigationController?.popViewController(animated: true)
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 
     /*
