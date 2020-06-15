77b71c209aaf32133019a8a301b03ffe61b27020
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2020-06-15 10:22:45 -0400

    Missing files

---
+++ amplify/generated/models/Note+Schema.swift
@@ -0,0 +1,39 @@
+// swiftlint:disable all
+import Amplify
+import Foundation
+
+extension Note {
+  // MARK: - CodingKeys 
+   public enum CodingKeys: String, ModelKey {
+    case id
+    case noteId
+    case title
+    case note
+    case category
+    case noteDate
+    case noteTime
+    case location
+    case user
+  }
+  
+  public static let keys = CodingKeys.self
+  //  MARK: - ModelSchema 
+  
+  public static let schema = defineSchema { model in
+    let note = Note.keys
+    
+    model.pluralName = "Notes"
+    
+    model.fields(
+      .id(),
+      .field(note.noteId, is: .required, ofType: .string),
+      .field(note.title, is: .required, ofType: .string),
+      .field(note.note, is: .required, ofType: .string),
+      .field(note.category, is: .required, ofType: .string),
+      .field(note.noteDate, is: .required, ofType: .string),
+      .field(note.noteTime, is: .required, ofType: .string),
+      .field(note.location, is: .required, ofType: .string),
+      .field(note.user, is: .required, ofType: .string)
+    )
+    }
+}
