57ad6b16dcad6f218306ab73d6285f0ae9f1bc29
Arthur Ataide <arthurreisataide@gmail.com>
2020-06-19 10:18:04 -0400

    Added the App Icon.  Starting the configuration of the Collection in notes Controller.

--- iNote/Controller/NoteViewController.swift
+++ iNote/Controller/NoteViewController.swift
@@ -11,29 +11,59 @@ import Amplify
 import AmplifyPlugins
 import Combine
 
-class NoteViewController: UIViewController {
+class NoteViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     //@State var todoSubscription: AnyCancellable?
-    var username:String?
+    @IBOutlet weak var collectionView: UICollectionView!
+    //var username:String?
     
     let notes:[Note] = []
+    let sections:[String] = ["School", "IOS", "Android"]
     
+    let headerID = "LessonHeader"
+    let cellID = "LessonCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        //title = "Notes"
-        // Do any additional setup after loading the view.
         navigationItem.hidesBackButton = true
         
+        collectionView.delegate = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionInset = UIEdgeInsets(top:10, left:20, bottom:10, right:20)
+        layout.headerReferenceSize = CGSize(width: 0,height: 40)
+        layout.itemSize = CGSize(width: 70,height: 45)
+        collectionView.register(UINib(nibName: self.cellID, bundle: nil), forCellWithReuseIdentifier: self.cellID)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,withReuseIdentifier: self.headerID)
+        collectionView.backgroundColor = .white
+        collectionView.contentInsetAdjustmentBehavior = .always
+        
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        performSegue(withIdentifier: "newNoteSegue", sender: self)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return notes.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell( withReuseIdentifier: self.cellID, for: indexPath)
+        let lab = cell.viewWithTag(1) as! UILabel
+        if lab.text == "Label" {
+            lab.highlightedTextColor = .white
+            cell.backgroundColor = .blue
+            cell.layer.borderColor = UIColor.brown.cgColor
+            cell.layer.borderWidth = 5
+            cell.layer.cornerRadius = 5
+        let v = UIView()
+            v.backgroundColor = UIColor.blue.withAlphaComponent(0.8)
+            cell.selectedBackgroundView = v
+        }
+        let term = self.sections[indexPath.section]
+        //lab.text = notes.tirl + term.sectionFirstWord
+        return cell
+    }
     
-    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
     
 //    func subscribeTodos() {
 //       self.todoSubscription
